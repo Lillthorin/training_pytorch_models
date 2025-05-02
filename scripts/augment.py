063c071e25cae2c4c506ae9053d181dba37c4222
Lillthorin <thorin.mathias@gmail.com>
2025-05-02 16:36:59 +0200

    Update augment.py

--- scripts/augment.py
+++ scripts/augment.py
@@ -3,31 +3,23 @@ from albumentations.pytorch import ToTensorV2
 
 
 def get_train_transform(imgsz):
-    return A.Compose([
+       return A.Compose([
         A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.3),
-        A.RandomRotate90(p=0.5),
         A.ShiftScaleRotate(
-            shift_limit=0.05,
-            scale_limit=0.2,
-            rotate_limit=45,
-            p=0.7,
+            shift_limit=0.03,
+            scale_limit=0.1,
+            rotate_limit=15,
+            p=0.5,
             border_mode=0
         ),
-        A.RandomBrightnessContrast(p=0.5),
-        A.HueSaturationValue(p=0.5),
-        A.RGBShift(p=0.4),
-        A.GaussNoise(p=0.2),
-        A.MotionBlur(blur_limit=3, p=0.2),
-        A.Perspective(scale=(0.05, 0.1), p=0.2),
+        A.RandomBrightnessContrast(p=0.3),
+        A.ColorJitter(p=0.3),
         A.Resize(imgsz, imgsz, p=1),
         A.Normalize(mean=(0.485, 0.456, 0.406),
                     std=(0.229, 0.224, 0.225)),
-        
-        
         ToTensorV2()
     ],
-    bbox_params=A.BboxParams(
+     bbox_params=A.BboxParams(
         format='pascal_voc',
         min_visibility=0.3,
         label_fields=['class_labels'],
